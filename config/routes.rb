27b3152c5d2585b3328f0aec35a1aa592b8bc1d5
Stan Shore <stan@workflowinc.com>
2011-07-26 18:42:01 -0700

    Finished first cut of the user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 SampleApp::Application.routes.draw do
-  # get "users/new"
-
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
+  resources :users
   root :to => "pages#home"
   match '/contact', :to => 'pages#contact'
   match '/about',   :to => 'pages#about'
